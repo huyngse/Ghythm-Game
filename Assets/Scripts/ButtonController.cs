e65f449ff2f5db10037f11694f45286b7de09785
huyngse <ngogiahuy775309@gmail.com>
2024-09-14 17:53:45 +0700

    Add button controller script

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private Sprite defaultImage;
+    [SerializeField]
+    private Sprite pressedImage;
+    [SerializeField]
+    private KeyCode keyToPress;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress)) {
+            spriteRenderer.sprite = pressedImage;
+        }
+        if (Input.GetKeyUp(keyToPress)) {
+            spriteRenderer.sprite = defaultImage;
+        }
+    }
+}
